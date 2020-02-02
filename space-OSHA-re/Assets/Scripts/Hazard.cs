f754c432e6e1c6b30756dbf1cd68007678c8c203
Tyler W <wallacet@email.arizona.edu>
2020-02-01 19:49:55 -0500

    Added hazard spawning system

---
+++ space-OSHA-re/Assets/Scripts/Hazard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    public static int hazardsFound { get; private set; }
+
+    public UnityEngine.Events.UnityEvent onFound;
+
+    private bool playerInArea;
+    private bool found;
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!found && playerInArea && Input.GetKeyDown("e"))
+        {
+            found = true;
+            hazardsFound++;
+            onFound.Invoke();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player entered");
+            playerInArea = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            playerInArea = false;
+        }
+    }
+}
---
+++ space-OSHA-re/Assets/Scripts/HazardSpawnPoint.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardSpawnPoint : MonoBehaviour
+{
+    [SerializeField, Range(0,100f)] private float xRange = 0;
+    [SerializeField, Range(0,100f)] private float yRange = 0;
+
+    [SerializeField] private HazardList hazards;
+
+    private bool alreadySpawned => currentHazard != null;
+
+    private Hazard currentHazard;
+
+    public void Spawn()
+    {
+        if (alreadySpawned) return;
+
+        var go = Instantiate(hazards.hazards[Random.Range(0, hazards.hazards.Count)].gameObject, transform);
+        currentHazard = go.GetComponent<Hazard>();
+        currentHazard.transform.localPosition
+            = new Vector3(
+                Random.Range(-xRange, xRange),
+                Random.Range(-yRange, yRange),
+                0);
+
+        currentHazard.transform.rotation = Quaternion.identity;
+
+        currentHazard.onFound.AddListener(OnHazardFound);
+    }
+
+    private void OnHazardFound()
+    {
+        currentHazard.onFound.RemoveListener(OnHazardFound);
+        currentHazard = null;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(xRange, yRange, 0));
+    }
+}
