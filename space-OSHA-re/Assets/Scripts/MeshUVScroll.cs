d89968e621265a2d0ad5a4dc4f9e258b435b1181
Tyler W <wallacet@email.arizona.edu>
2020-02-02 10:53:54 -0500

    Added a star field prefab with movement

---
+++ space-OSHA-re/Assets/Scripts/MeshUVScroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshUVScroll : MonoBehaviour
+{
+    [SerializeField] private Vector2 scrollSpeed;
+
+    private Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.mainTextureOffset += scrollSpeed * Time.deltaTime;
+    }
+}
