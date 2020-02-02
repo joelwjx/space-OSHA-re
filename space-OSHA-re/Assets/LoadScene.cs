4863dbe57a7823f66f305763dda68be9810a98cd
Tyler W <wallacet@email.arizona.edu>
2020-02-02 15:51:36 -0500

    Setup build settings, connected intro scene to the rest of the game.

---
+++ space-OSHA-re/Assets/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScene : StateMachineBehaviour
+{
+     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1); // hardcode because gamejam
+    }
+
+}
