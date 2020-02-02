852492d6b5693a11f29bf78a27197a0af94f7785
Tyler W <wallacet@email.arizona.edu>
2020-02-02 15:36:49 -0500

    Added intro scene and assets

---
+++ space-OSHA-re/Assets/SpriteSheetsIntroScene/PlayAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayAudio : MonoBehaviour
+{
+    [SerializeField] private AudioClip clipToPlay;
+    [SerializeField] private float delayTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayRoutine());
+    }
+
+    private IEnumerator PlayRoutine()
+    {
+        yield return new WaitForSeconds(delayTime);
+        var source = GetComponent<AudioSource>();
+        source.clip = clipToPlay;
+        source.Play();
+    }
+}
